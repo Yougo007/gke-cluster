89bcd3177783e8f3e9240ed7c6515ee779c8278f
Yougo007 <udokporo_ugochukwu@yahoo.co.uk>
2021-02-10 14:22:57 -0500

    reverting change to support terraform version >.14

--- environments/dev/gke.tf
+++ environments/dev/gke.tf
@@ -16,7 +16,7 @@ variable "gke_num_nodes" {
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
-  location = "${var.region}"
+  location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -37,7 +37,7 @@ resource "google_container_cluster" "primary" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = "${var.region}"
+  location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
