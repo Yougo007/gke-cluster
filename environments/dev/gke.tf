79fed8062812db0da55f09efbbd2fe1ff3787890
Yougo007 <udokporo_ugochukwu@yahoo.co.uk>
2021-07-06 09:42:20 -0400

    correct node count typo

--- environments/dev/gke.tf
+++ environments/dev/gke.tf
@@ -19,7 +19,7 @@ resource "google_container_cluster" "primary" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 2 2 
+  initial_node_count       = 1  
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
