16c4ddf828ea30ae87a0d678654be4b527259649
Yougo007 <33158785+Yougo007@users.noreply.github.com>
2021-10-04 11:36:52 -0400

    Update vpc.tf

--- environments/dev/vpc.tf
+++ environments/dev/vpc.tf
@@ -13,13 +13,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.project_id}-${var.region}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.project_id}-${var.region}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
