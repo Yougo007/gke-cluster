40058256d99522e43df2faa84ba85d0e6cb772b1
Yougo007 <33158785+Yougo007@users.noreply.github.com>
2022-11-22 17:16:04 -0500

    Update gke.tf

--- environments/dev/gke.tf
+++ environments/dev/gke.tf
@@ -44,6 +44,7 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
 
     labels = {
